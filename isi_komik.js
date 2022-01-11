8b91f9f3e2cdde948219c3ec9f89f4c11811c56a
TryBugger <63944068+TryBugger@users.noreply.github.com>
2022-01-11 15:54:22 +0800

    Add files via upload
    Nambah JavaScript Nampilin Gambar

--- isi_komik.js
+++ isi_komik.js
@@ -1,8 +1,15 @@
+//  function chapterDropDown() {
+gambar = document.getElementById("chapterGambar");
+console.log("tes");
+for(let i = 1; i <= 2; i++) {
+   gambar.innerHTML += `
+      <img src="img/komikdummy/${i}.png" alt="">
+   `;
+   console.log("tes gambar");
+}
+//  }
+
 function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
- }
-
- function chapterDropDown() {
-    //  fungsi chapter dropdown
  }
