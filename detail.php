6a9c9d814d7936a9c62fa95888a2be8a58aade44
Aryanda Rizky <aryandarizky1234@gmail.com>
2022-01-15 10:24:51 +0700

    in

--- detail.php
+++ detail.php
@@ -1,37 +1,36 @@
 <?php
-    require 'db/functions.php';
+require 'db/functions.php';
 
-    $id = $_GET['id'];
-    
-    $select_komik = mysqli_query($conn, "SELECT * FROM `komik` WHERE komik_id = $id");
-    $data_detail = mysqli_fetch_assoc($select_komik);
+$id = $_GET['id'];
 
-    $select_list_genre = mysqli_query($conn, "SELECT * FROM `list_genre` JOIN `genre` ON list_genre.genre_id = genre.genre_id WHERE list_genre.komik_id = $id");
-    $data_genre_list = [];
-    while($row_list = mysqli_fetch_assoc($select_list_genre)) {
-        $data_genre_list[] = $row_list;
-    }
+$select_komik = mysqli_query($conn, "SELECT * FROM `komik` WHERE komik_id = $id");
+$data_detail = mysqli_fetch_assoc($select_komik);
 
-    $select_komik_chapter = mysqli_query($conn, "SELECT * from `chapter` JOIN `komik` ON chapter.komik_id = komik.komik_id WHERE chapter.komik_id = $id");
-    $data_chapter = [];
-    while($row_list = mysqli_fetch_assoc($select_komik_chapter)) {
-        $data_chapter[] = $row_list;
-    }
+$select_list_genre = mysqli_query($conn, "SELECT * FROM `list_genre` JOIN `genre` ON list_genre.genre_id = genre.genre_id WHERE list_genre.komik_id = $id");
+$data_genre_list = [];
+while ($row_list = mysqli_fetch_assoc($select_list_genre)) {
+    $data_genre_list[] = $row_list;
+}
 
-    // var_dump($data_chapter[0]["nama_chapter"]);
+$select_komik_chapter = mysqli_query($conn, "SELECT * from `chapter` JOIN `komik` ON chapter.komik_id = komik.komik_id WHERE chapter.komik_id = $id");
+$data_chapter = [];
+while ($row_list = mysqli_fetch_assoc($select_komik_chapter)) {
+    $data_chapter[] = $row_list;
+}
 
-    var_dump($data_detail["total_chapter"])
+// var_dump($data_chapter[0]["nama_chapter"]);
+
+var_dump($data_detail["total_chapter"])
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
     <title>Detail Komik</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <style>
@@ -94,7 +93,7 @@
     <ul class="tulisanNav">
         <li><img src="img/logo.webp" width="200px" alt=""></li>
         <li><a href="index.php">Home</a></li>
-        <li><a href="genre.html">Genre</a></li>
+        <li><a href="genre.php">Genre</a></li>
         <li><a href="release.html">Latest Release</a></li>
         <label class="switch" style="margin-top: 12px; float: right; margin-right: 10px;">
             <input type="checkbox" onclick="darkMode()">
@@ -115,8 +114,8 @@
             <p class="title"><strong><?= $data_detail["nama_komik"] ?></strong></p>
             <p>
                 Genre :
-                <?php foreach($data_genre_list as $data) { ?>
-                    <button class="btn btn-secondary btn-sm me-3"><?= ucwords($data["nama_genre"]) ?></button>        
+                <?php foreach ($data_genre_list as $data) { ?>
+                    <button class="btn btn-secondary btn-sm me-3"><?= ucwords($data["nama_genre"]) ?></button>
                 <?php } ?>
             </p>
             <table border="0" cellpadding="0">
@@ -147,7 +146,7 @@
             <hr>
             <div class="scroll">
                 <table cellpadding="5" id="table">
-                    <?php foreach($data_chapter as $chapter) {?>
+                    <?php foreach ($data_chapter as $chapter) { ?>
                         <tr>
                             <td style="padding-right: 850px;">
                                 <a href="isi_komik.php?id=<?= $chapter["chapter_id"] ?>"><?= $chapter["nama_chapter"] ?></a>
@@ -160,26 +159,7 @@
 
         </div>
     </div>
-    <!-- <div class="komik_info-cover-box">
-        <div class="komik_info-cover-image">
-          <img width="824" height="1172" src="https://komikcast.com/wp-content/uploads/2021/07/E6p48uFUYAM9A_3-e1626830020974.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" loading="lazy">      </div>
-      </div> -->
+
 </body>
-<!-- <script>
-    console.log("tes");
-    var table = document.getElementById('table');    
-    var i =3;
-    while (i != 0) {
-        table.innerHTML += `
-                    <tr>
-                        <td style="padding-right: 850px;">
-                            <a href="isi_komik.html">Chapter ${i}</a>
-                        </td>
-                        <td>2 weeks ago</td>
-                    </tr>`
-        i -= 1;
-    }
-</script> -->
-<!-- <script src="isi_komik.js"></script> -->
 
 </html>
