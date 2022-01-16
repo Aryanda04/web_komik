5c7d814906e09ed6d56bf850b425dce8845d65d1
DarkVixion <hafizhfuq12@gmail,com>
2022-01-16 20:09:46 +0700

    lupa apa yg di update

--- Release.php
+++ Release.php
@@ -62,7 +62,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="Lrelase.js"></script>
-    <!-- <script src="isi_komik.js"></script> -->
+    <script src="isi_komik.js"></script>
 </body>
 
 </html>
