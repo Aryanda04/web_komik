a18753d0ffe027785bde220a1309ea202625423c
DarkVixion <hafizhfuq12@gmail,com>
2022-01-19 00:14:06 +0700

    db correct

--- Release.php
+++ Release.php
@@ -1,8 +1,10 @@
 <?php
 require 'db/functions.php';
 
-$data_komik = selectALL("SELECT komik.*,MAX(chapter.waktu_update) AS waktu_update,total_views,COUNT(komik.komik_id) AS total_chapter FROM `komik` JOIN `chapter` ON komik.komik_id = chapter.komik_id GROUP BY komik.komik_id");
-$select_komik_sort_view = "SELECT komik.*, COUNT(komik.komik_id) AS total_chapter FROM komik JOIN chapter ON komik.komik_id = chapter.komik_id GROUP BY komik.komik_id ORDER BY waktu_update DESC";function showKomik($queryKomik, $loop)
+$data_komik = "SELECT komik.*,MAX(chapter.waktu_update) AS waktu_update,total_views,COUNT(komik.komik_id) AS total_chapter FROM `komik` JOIN `chapter` ON komik.komik_id = chapter.komik_id GROUP BY komik.komik_id ORDER BY waktu_update DESC";
+
+
+function showKomik($queryKomik, $loop)
 {
   $i = 1;
   // data list komik berdasarkan query dari variabel queryKomik
@@ -19,7 +21,7 @@ $select_komik_sort_view = "SELECT komik.*, COUNT(komik.komik_id) AS total_chapte
     echo "<div class=\"list-produk\">";
     echo "  <div class=\"kategori\">";
     echo "    <div>" . $komik["kategori"] . "</div>";
-    echo "    <div>" . $komik["waktu_update"] . " Views</div>";
+    echo "    <div>" . $komik["waktu_update"] . "</div>";
     echo "  </div>";
     echo "  <div class=\"cover\">";
     echo "  <img src=\"img/" . $komik["nama_komik"] . "/" . $komik["cover_komik"] . "\">";
@@ -119,7 +121,7 @@ $select_komik_sort_view = "SELECT komik.*, COUNT(komik.komik_id) AS total_chapte
 
 
             <div class="flex-container" id="most-view">
-                <?php showKomik($select_komik_sort_view, -1) ?>
+                <?php showKomik($data_komik, -1) ?>
             </div>
         </div>
     </div>
