45f632d6bd55f5effb81216f952b02f70b14031e
DarkVixion <hafizhfuq12@gmail,com>
2022-01-19 23:24:17 +0700

    ganti isi view jadi waktu update + new php reff

---
+++ php/functionsrelease.php
@@ -0,0 +1,27 @@
+<?php
+
+function komikCard($komik) {
+  $max_char = 100;
+    if (strlen($komik["deskripsi"]) > $max_char) {
+      $deskripsi = substr($komik["deskripsi"], 0, $max_char) . "...";
+    } else {
+      $deskripsi = $komik["deskripsi"];
+    }
+
+    echo "<div class=\"list-produk\">";
+    echo "  <div class=\"kategori\">";
+    echo "    <div>" . $komik["kategori"] . "</div>";
+    echo "    <div>" . $komik["waktu_update"] . "</div>";
+    echo "  </div>";
+    echo "  <div class=\"cover\">";
+    echo "  <img src=\"img/" . $komik["nama_komik"] . "/" . $komik["cover_komik"] . "\">";
+    echo "  </div>";
+    echo "    <div class=\"judul\">" . $komik["nama_komik"] . "</div>";
+    echo "    <p class=\"deskripsi\">" . $deskripsi . "</p>";
+    echo "  <div class=\"footer\">";
+    echo "    <a href=\"detail.php?id=" . $komik["komik_id"] . "\" class=\"detail-button\"> Details </a>";
+    echo "    <div class=\"detail-chapter\">" . $komik["total_chapter"] . " Chapter</div>";
+    echo "  </div>";
+    echo "</div>";
+}
+?>
