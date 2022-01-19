6352107707f9c97756e75c4646b8ffc6eedda13d
Aryanda Rizky <aryandarizky1234@gmail.com>
2022-01-20 02:42:02 +0700

    INI UDAH KEREN GUYS

--- genre.js
+++ genre.js
@@ -1,65 +1,90 @@
-function change() {
-  var genres = document.querySelectorAll(".gen input[type='checkbox']");
-  var filters = {
-    gen: getClassOfCheckedCheckboxes(genres),
-  };
-
-   filterResults(filters);
-}
-
-function getClassOfCheckedCheckboxes(checkboxes) {
-   var classes = [];
-
-  if (checkboxes && checkboxes.length > 0) {
-     for (var i = 0; i < checkboxes.length; i++) {
-      var cb = checkboxes[i];
-
-       if (cb.checked) {
-        classes.push(cb.getAttribute("rel"));
-      }
-    }
+function darkMode() {
+  var element = document.body;
+  var element2 = document.getElementsByClassName("badan");
+  var element3 = document.getElementsByClassName("grid-container");
+  var element4 = document.getElementsByClassName("list-produk");
+  var element5 = document.getElementsByClassName("grid-container3");
+  var element6 = document.getElementsByClassName("grid-item");
+  element.classList.toggle("dark-mode");
+  console.log("test")
+  element5[0].classList.toggle("grid2-darkmode");
+  for(let item of element2) {
+      item.classList.toggle("badan-darkmode");
   }
-
-   return classes;
-}
-
- function filterResults(filters) {
-  var rElems = document.querySelectorAll(".result div");
-   var hiddenElems = [];
-
-   if (!rElems || rElems.length <= 0) {
-     return;
-   }
-
-   for (var i = 0; i < rElems.length; i++) {
-     var el = rElems[i];
-
-     if (filters.gen.length > 0) {
-      var isHidden = true;
-
-       for (var j = 0; j < filters.gen.length; j++) {
-         var filter = filters.gen[j];
-
-        if (el.classList.contains(filter)) {
-         isHidden = false;
-           break;           }
-       }
-
-       if (isHidden) {
-        hiddenElems.push(el);
-       }
-     }
-   }
-
-   for (var i = 0; i < rElems.length; i++) {
-     rElems[i].style.display = "block";
-   }
-
-  if (hiddenElems.length <= 0) {
-    return;
+  console.log("test")
+  for(let item of element3) {
+      item.classList.toggle("grid-darkmode");
   }
-
-  for (var i = 0; i < hiddenElems.length; i++) {
-    hiddenElems[i].style.display = "none";
- }
+  for(let item of element4){
+      item.classList.toggle("list-darkmode");
+  }
+  for(let item of element6) {
+    item.classList.toggle("gridItem-darkmode");
 }
+}
+
+// function change() {
+//   var genres = document.querySelectorAll(".gen input[type='checkbox']");
+//   var filters = {
+//     gen: getClassOfCheckedCheckboxes(genres),
+//   };
+
+//    filterResults(filters);
+// }
+
+// function getClassOfCheckedCheckboxes(checkboxes) {
+//    var classes = [];
+
+//   if (checkboxes && checkboxes.length > 0) {
+//      for (var i = 0; i < checkboxes.length; i++) {
+//       var cb = checkboxes[i];
+
+//        if (cb.checked) {
+//         classes.push(cb.getAttribute("rel"));
+//       }
+//     }
+//   }
+
+//    return classes;
+// }
+
+//  function filterResults(filters) {
+//   var rElems = document.querySelectorAll(".result div");
+//    var hiddenElems = [];
+
+//    if (!rElems || rElems.length <= 0) {
+//      return;
+//    }
+
+//    for (var i = 0; i < rElems.length; i++) {
+//      var el = rElems[i];
+
+//      if (filters.gen.length > 0) {
+//       var isHidden = true;
+
+//        for (var j = 0; j < filters.gen.length; j++) {
+//          var filter = filters.gen[j];
+
+//         if (el.classList.contains(filter)) {
+//          isHidden = false;
+//            break;           }
+//        }
+
+//        if (isHidden) {
+//         hiddenElems.push(el);
+//        }
+//      }
+//    }
+
+//    for (var i = 0; i < rElems.length; i++) {
+//      rElems[i].style.display = "block";
+//    }
+
+//   if (hiddenElems.length <= 0) {
+//     return;
+//   }
+
+//   for (var i = 0; i < hiddenElems.length; i++) {
+//     hiddenElems[i].style.display = "none";
+//  }
+    // }
--- genre.php
+++ genre.php
@@ -5,7 +5,7 @@ require 'php/functions.php';
 /* kumpulan data */
 // data list komik semua
 $data_komik = selectALL("SELECT komik.*,MAX(chapter.waktu_update) AS waktu_update,total_views,COUNT(komik.komik_id) AS total_chapter FROM `komik` JOIN `chapter` ON komik.komik_id = chapter.komik_id GROUP BY komik.komik_id");
-foreach($data_komik as $key => $komik) {
+foreach ($data_komik as $key => $komik) {
   $id = $komik["komik_id"];
   $list_genre_komik = array_column(selectALL("SELECT nama_genre FROM `genre` JOIN `list_genre` ON list_genre.genre_id = genre.genre_id WHERE list_genre.komik_id = $id"), "nama_genre");
   $data_komik[$key]["list_genre"] = $list_genre_komik;
@@ -17,28 +17,29 @@ $data_genre = selectALL("SELECT * FROM `genre`");
 
 function showKomik($list_komik, $loop)
 {
-    $i = 1;
-    foreach ($list_komik as $komik) {
-        if(isset($_GET["Genre"])) {
-            if(cekGenre($komik)) {
-              komikCard($komik);      
-            } else {
-              continue;
-            }
-        } else {
-          komikCard($komik);
-        }
-        $i++;
-        // var_dump($komik['komik_id']);
-        if ($i > $loop && $loop != -1) {
-            break;
-        }
+  $i = 1;
+  foreach ($list_komik as $komik) {
+    if (isset($_GET["Genre"])) {
+      if (cekGenre($komik)) {
+        komikCard($komik);
+      } else {
+        continue;
+      }
+    } else {
+      komikCard($komik);
     }
+    $i++;
+    // var_dump($komik['komik_id']);
+    if ($i > $loop && $loop != -1) {
+      break;
+    }
+  }
 }
 
-function cekGenre($komik) {
-  foreach($_GET["Genre"] as $pilihan_genre) {
-    if(in_array($pilihan_genre, $komik["list_genre"])) {
+function cekGenre($komik)
+{
+  foreach ($_GET["Genre"] as $pilihan_genre) {
+    if (in_array($pilihan_genre, $komik["list_genre"])) {
       return TRUE;
     }
   }
@@ -65,40 +66,23 @@ function cekGenre($komik) {
   <div class="badan">
     <div class="filter">
       <h1 class="grid-container">Genres</h1>
-      <div class="grid-container2">
-        <form method="GET">
-            <?php foreach($data_genre as $genre) { ?>
-              <div class="checkbox">
-                <label><input type="checkbox" name="Genre[]" rel="<?= $genre["nama_genre"] ?>" value="<?= $genre["nama_genre"] ?>"><?= ucwords($genre["nama_genre"]) ?></label>
-              </div>
-            <?php } ?>
-            <!-- <div class="checkbox">
-              <label><input type="checkbox" rel="action" onchange="change();" />Action</label>
-            </div>
 
-            <div class="checkbox">
-              <label><input type="checkbox" rel="adven" onchange="change();" />Adventure</label>
-            </div>
+      <form method="GET">
+        <div class="grid-container2">
+          <?php foreach ($data_genre as $genre) { ?>
 
-            <div class="checkbox">
-              <label><input type="checkbox" rel="comedy" onchange="change();" />Comedy</label>
-            </div>
 
-            <div class="checkbox">
-              <label><input type="checkbox" rel="fantasy" onchange="change();" />Fantasy</label>
-            </div>
+            <label class="grid-item"><input type="checkbox" name="Genre[]" rel="<?= $genre["nama_genre"] ?>" value="<?= $genre["nama_genre"] ?>"><?= ucwords($genre["nama_genre"]) ?></label>
 
-            <div class="checkbox">
-              <label><input type="checkbox" rel="romance" onchange="change();" />Romance</label>
-            </div>
 
-            <div class="checkbox">
-              <label><input type="checkbox" rel="Horror" onchange="change();" />Horror</label>
-            </div> -->
-            <input type="submit" value="Pilih Genre">
-        </form>
-      </div>
+          <?php } ?>
+
+        </div>
+        <div class="grid-container3">
+          <input class="detail-button" type="submit" value="Pilih Genre">
+        </div>
     </div>
+  </div>
   </div>
 
   <div class="badan">
@@ -107,7 +91,7 @@ function cekGenre($komik) {
     </div>
   </div>
   <script src="genre.js"></script>
-  <script src="isi_komik.js"></script>
+
 </body>
 
 </html>
