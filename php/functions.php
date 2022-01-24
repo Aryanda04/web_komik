94650a975eba793f7465d95fa86e16c81260f326
Aryanda Rizky <aryandarizky1234@gmail.com>
2022-01-24 09:21:31 +0700

    mantap broo, dah jadi

--- php/functions.php
+++ php/functions.php
@@ -1,27 +1,33 @@
 <?php
 
-function komikCard($komik) {
+function komikCard($komik)
+{
   $max_char = 100;
-    if (strlen($komik["deskripsi"]) > $max_char) {
-      $deskripsi = substr($komik["deskripsi"], 0, $max_char) . "...";
-    } else {
-      $deskripsi = $komik["deskripsi"];
-    }
+  if (strlen($komik["deskripsi"]) > $max_char) {
+    $deskripsi = substr($komik["deskripsi"], 0, $max_char) . "...";
+  } else {
+    $deskripsi = $komik["deskripsi"];
+  }
 
-    echo "<div class=\"list-produk\">";
-    echo "  <div class=\"kategori\">";
-    echo "    <div>" . $komik["kategori"] . "</div>";
-    echo "    <div>" . $komik["total_views"] . " Views</div>";
-    echo "  </div>";
-    echo "  <div class=\"cover\">";
-    echo "  <img src=\"img/" . $komik["nama_komik"] . "/" . $komik["cover_komik"] . "\">";
-    echo "  </div>";
-    echo "    <div class=\"judul\">" . $komik["nama_komik"] . "</div>";
-    echo "    <p class=\"deskripsi\">" . $deskripsi . "</p>";
-    echo "  <div class=\"footer\">";
-    echo "    <a href=\"detail.php?id=" . $komik["komik_id"] . "\" class=\"detail-button\"> Details </a>";
-    echo "    <div class=\"detail-chapter\">" . $komik["total_chapter"] . " Chapter</div>";
-    echo "  </div>";
-    echo "</div>";
+?>
+
+  <div class="list-produk">
+    <div class="kategori">
+      <div><?= $komik["kategori"] ?></div>
+      <div><?= $komik["total_views"] ?> Views</div>
+    </div>
+    <div class="cover">
+      <a href="detail.php?id= <?= $komik["komik_id"] ?>"><img src="img/<?= $komik["nama_komik"] ?>/<?= $komik["cover_komik"] ?>"></a>
+
+    </div>
+    <a class="judul" href="detail.php?id= <?= $komik["komik_id"] ?>">
+      <div><?= $komik["nama_komik"] ?></div>
+    </a>
+    <p class="deskripsi"><?= $deskripsi ?></p>
+    <div class="footer">
+      <div class="detail-chapter"><?= $komik["total_chapter"] ?> Chapter</div>
+    </div>
+  </div>
+<?php
 }
 ?>
